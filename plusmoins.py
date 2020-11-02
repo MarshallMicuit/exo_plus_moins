fd5e52a7a09de2cf2941dbc308c5de886d8eb461
MarshallMicuit <73831113+MarshallMicuit@users.noreply.github.com>
2020-11-02 14:19:11 +0100

    Add files via upload

--- plusmoins.py
+++ plusmoins.py
@@ -4,22 +4,22 @@ en lui donnant des indices 'supérieur ou inférieur'"""
 # On définit un int aléatoire
 from random import randint
 but = randint(0,100)
-win = False
+victoire = False
 vie = 5
 
 # On a notre chiffre et notre variable condition de victoire créés, on lance la boucle de comparaison
-while win == False and vie != 0:
+while not victoire and vie > 0:
     print("Vie(s) : ", vie, "\n")
-    nmb_ut = input("Choisissez un nombre :")
-    nmb_ut = int(nmb_ut)
-    if nmb_ut < but:
+    nombre_utilisateur = input("Choisissez un nombre :")
+    nombre_utilisateur = int(nombre_utilisateur)
+    if nombre_utilisateur < but:
         print("C'est trop petit mon con")
         vie = vie - 1
-    elif nmb_ut > but:
+    elif nombre_utilisateur > but:
         print("C'est trop grand mon con")
         vie = vie - 1
-    elif nmb_ut == but:
+    else:
         print("C'est gagné, yes we did it !")
-        win = True
+        victoire = True
 if vie == 0:
         print("La mort, la mort, la moooooort")
